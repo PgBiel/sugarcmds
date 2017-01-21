f6dcebb5582536a4a1f1426694dd01892acab8e0
PgBiel <pgbiel@hotmail.com.br>
2017-01-20 23:12:26 -0200

    I screwed up

--- cmds.js
+++ cmds.js
@@ -2,10 +2,11 @@
 const Discord = require("discord.js");
 const Storage = require("saltjs").Storage;
 const fs = require("fs");
+const path = require("path");
 let loadedmodules = {};
-for (let file of fs.readdirSync("./src")) {
+for (let file of fs.readdirSync(path.join(__dirname, "src"))) {
   if (/\.js$/.test(file)) {
-    loadedmodules[file.replace(/\.js$/, "")] = require(`./src/${file}`);
+    loadedmodules[file.replace(/\.js$/, "")] = require(path.join(__dirname, `src/${file}`));
   }
 }
 
