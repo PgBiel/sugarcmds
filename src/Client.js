e70c11ea9df26c5e1f0bc0f7f386e7870e6223a0
PgBiel <pgbiel@hotmail.com.br>
2017-01-21 00:36:40 -0200

    Gotta start working

--- src/Client.js
+++ src/Client.js
@@ -1,6 +1,7 @@
 const Discord = require("discord.js");
 const Send = require("./Send.js");
 const Storage = require("saltjs").Storage;
+const CommandHandler = requie("./CommandHandler.js");
 Client = module.exports = class Client extends Discord.Client {
   constructor(options) {
     super(options);
@@ -12,5 +13,31 @@ Client = module.exports = class Client extends Discord.Client {
         c.sr = c.sender; //Alias lol
       });
     });
+    this.on("channelCreate", c=>{
+      c.sender = new Send(c, this);
+      c.sr = c.sender;
+    });
+    let { defaultPrefix, prefixes, ownerID } = options;
+    defaultPrefix = defaultPrefix || "!";
+    this.prefixes = new Storage();
+    if (typeof defaultPrefix !== "string") defaultPrefix = defaultPrefix.toString();
+    if (typeof ownerID !== "string" && !(ownerID instanceof Array)) throw new TypeError("Must give an owner ID or array of owner IDs.");
+    if (prefixes) {
+      if (typeof prefixes !== "object") throw new TypeError("Prefixes must be an object of format `guildID: prefix` or a Storage mapping prefixes by guild ID.");
+      if (prefixes instanceof Storage) {
+        this.prefixes = prefixes;
+      } else {
+        for (let guildID in prefixes) {
+          this.prefixes.set(guildID.toString(), prefixes[guildID].toString()); //in case they aren't strings for some weird reason
+        }
+      }
+    }
+    this.owners = ownerID;
+  }
+
+  reloadPrefixes(prefixes) {
+    if (!prefixes || typeof prefixes !== "object") throw new TypeError("Prefixes must be an object of format `guildID: prefix` or a Storage mapping prefixes by guild ID.");
+    this.prefixes = prefixes;
+    return true;
   }
 };
